9cae233c63b17f37102c8bb1912d674814e044fa
fernando <s24h@outlook.es>
2025-01-26 10:44:33 -0500

    Primera versión de Kits Cámaras

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
