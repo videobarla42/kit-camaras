677be45a144afcb0c1556451053a8ee3e2eb3052
fernando <s24h@outlook.es>
2025-01-26 11:28:29 -0500

    Primera versión de Kits Cámaras

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import App from "./App";
 import "./index.css";
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { HashRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
