10536f0639c9d8dcba24f57c58735865e4c73dc4
fernando <s24h@outlook.es>
2025-01-26 04:13:47 -0500

    Primera versión de Kits Cámaras

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Kits_8_DS_2CE56D0T_IRPF_C from './pages/kits/kits_8_DS-2CE56D0T-IRPF(C)/kits_8_DS-2CE56D0T-IRPF(C).tsx'; // Ruta correcta para Home
 import Kit_4_DS_2CE56D0T_IRPF_C from './pages/kits/kit_4_DS-2CE56D0T-IRPF(C)/kit_4_DS-2CE56D0T-IRPF(C).tsx'; // Ruta correcta para el componente 'About'
 import './App.css'; // Importa el archivo CSS
