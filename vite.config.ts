4a2bb81c4671bde2d4c95754b17733c9efa6c7a4
fernando <s24h@outlook.es>
2025-01-27 07:29:12 -0500

    Primera versión de Kits Cámaras

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/kits_camaras/", // Nombre del repositorio
+  base: "/", // Para dominios personalizados, usa "/"
   resolve: {
     alias: {
-      '@': '/src',  // Esto hace que '@' apunte a la carpeta 'src'
+      '@': '/src', // Alias para que '@' apunte a 'src'
     },
   },
 });
