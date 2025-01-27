2fe035b64e6794e0daf4e593339bbd519cda85e3
fernando <s24h@outlook.es>
2025-01-27 02:41:51 -0500

    Primera versión de Kits Cámaras

--- src/pages/kits/kit_4_DS-2CE56D0T-IRPF(C)/kit_4_DS-2CE56D0T-IRPF(C).tsx
+++ src/pages/kits/kit_4_DS-2CE56D0T-IRPF(C)/kit_4_DS-2CE56D0T-IRPF(C).tsx
@@ -7,17 +7,34 @@ import Beneficios from '@/componentes/Beneficios.tsx';
 import Banner from '@/componentes/Banner';
 import Formulario from '@/componentes/Formulario';
 
-// Importar imágenes locales
-import imagen1 from '@/assets/1.png';
-import imagen2 from '@/assets/2.png';
-import imagen3 from '@/assets/3.png';
-import imagen4 from '@/assets/4.png';
-import imagen5 from '@/assets/5.png';
-import imagen6 from '@/assets/6.png';
-import imagen7 from '@/assets/7.png';
-import imagen8 from '@/assets/8.png';
-import imagen9 from '@/assets/9.png';
-import headerImage from '@/assets/1.png';
+// Importar imágenes locales en formato .webp
+import imagen1 from '@/assets/1.webp';
+import imagen2 from '@/assets/2.webp';
+import imagen3 from '@/assets/3.webp';
+import imagen4 from '@/assets/4.webp';
+import imagen5 from '@/assets/5.webp';
+import imagen6 from '@/assets/6.webp';
+import imagen7 from '@/assets/7.webp';
+import imagen8 from '@/assets/8.webp';
+import imagen9 from '@/assets/9.webp';
+import imagen10 from '@/assets/10.webp';
+import imagen11 from '@/assets/11.webp';
+import imagen12 from '@/assets/12.webp';
+import imagen13 from '@/assets/13.webp';
+import imagen14 from '@/assets/14.webp';
+import imagen15 from '@/assets/15.webp';
+import imagen16 from '@/assets/16.webp';
+import imagen17 from '@/assets/17.webp';
+import imagen18 from '@/assets/18.webp';
+import imagen19 from '@/assets/19.webp';
+import imagen20 from '@/assets/20.webp';
+import imagen21 from '@/assets/21.webp';
+import imagen22 from '@/assets/22.webp';
+import imagen23 from '@/assets/23.webp';
+import imagen24 from '@/assets/24.webp';
+import imagen25 from '@/assets/25.webp';
+import imagen26 from '@/assets/26.webp';
+import headerImage from '@/assets/1.webp'; // También actualizada a .webp
 
 const Kit4Camaras: React.FC = () => {
   const [modalAbierto, setModalAbierto] = useState(false);
@@ -47,43 +64,43 @@ const Kit4Camaras: React.FC = () => {
     {
       title: 'DVR iDS-7104HQHI-M1/S 4 Mpx Lite',
       description: 'DVR AcuSense mini 1U H.265 de 4 canales 1080P con detección de movimiento avanzada (humanos y vehículos) y protección perimetral. Incluye compresión H.265 Pro+, soporte para cámaras IP de hasta 6 MP, y grabación en múltiples resoluciones hasta 4mpx Lite.',
-      images: [imagen1, imagen2, imagen3],
+      images: [imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8],
       botonTexto: 'Comprar DVR AcuSense'
     },
     {
       title: '4 Camaras de 2 Mpx X 1 Año de Garantia',
       description: '4 Cámaras Domo TURBOHD 1080p de HIKVISION con lente de 2.8 mm y visión nocturna inteligente EXIR de hasta 20 mts.',
-      images: [imagen4, imagen5, imagen6],
+      images: [imagen9, imagen10, imagen11, imagen12, imagen13, imagen14, imagen15, imagen16, imagen17],
       botonTexto: 'Comprar Sistema Completo'
     },
     {
       title: 'Disco duro para videovigilancia WD Purple - 1 TB',
       description: 'Disco duro WD Purple 1 TB para videovigilancia, grabación 24/7, tecnología AllFrame, velocidad 110 MB/s, interfaz SATA, caché 64 MB, soporta 64 cámaras.',
-      images: [imagen7, imagen8, imagen9],
+      images: [imagen18, imagen19, imagen20, imagen21, imagen22],
       botonTexto: 'Comprar Cámaras Adicionales'
     },
     {
       title: '4 Fuentes De Energia 12v - 1amp',
       description: 'fuente de alimentación de AC 110- 240v, 12 voltios a 1 amperio de alta calidad, conector de alimentación de 2.1mm, con led indicador de funcionamiento, el articulo puede ser ligeramente distinto al de la foto.',
-      images: [imagen7, imagen8, imagen9],
+      images: [imagen23],
       botonTexto: 'Comprar Cámaras Adicionales'
     },
     {
       title: '4 pares de Video Baluns 12v - 1amp',
       description: '4 Pares de VIDEO BALUM TURBO 4 EN 1 transmisión de señales de video a través de cables UTP, señal clara y estable a larga distancia.',
-      images: [imagen7, imagen8, imagen9],
+      images: [imagen24],
       botonTexto: 'Comprar Cámaras Adicionales'
     },
     {
       title: '4 pares de Borneras',
       description: '4 pares de borneras electricas para la conexión y distribución de energia de las camaras.',
-      images: [imagen7, imagen8, imagen9],
+      images: [imagen25],
       botonTexto: 'Comprar Cámaras Adicionales'
     },
     {
       title: '100 mts de cable utp',
       description: 'cable utp cat 5e en aleacion para interior inluidos en el kit.',
-      images: [imagen7, imagen8, imagen9],
+      images: [imagen26],
       botonTexto: 'Comprar Cámaras Adicionales'
     },
   ];
@@ -115,37 +132,37 @@ const Kit4Camaras: React.FC = () => {
       padding="15px 30px"
     />,
     <Banner
-    key="banner1"
-    texto="Oferta especial: 50% de descuento"
-    backgroundColor="#FF5722"
-    textColor="#FFFFFF"
-    fontSize="2rem"
-    padding="15px 30px"
-  />,
-  <Banner
-  key="banner1"
-  texto="Oferta especial: 50% de descuento"
-  backgroundColor="#FF5722"
-  textColor="#FFFFFF"
-  fontSize="2rem"
-  padding="15px 30px"
-/>,
- <Banner
- key="banner1"
- texto="Oferta especial: 50% de descuento"
- backgroundColor="#FF5722"
- textColor="#FFFFFF"
- fontSize="2rem"
- padding="15px 30px"
-/>,
- <Banner
- key="banner1"
- texto="Oferta especial: 50% de descuento"
- backgroundColor="#FF5722"
- textColor="#FFFFFF"
- fontSize="2rem"
- padding="15px 30px"
-/>,
+      key="banner4"
+      texto="Oferta especial: 50% de descuento"
+      backgroundColor="#FF5722"
+      textColor="#FFFFFF"
+      fontSize="2rem"
+      padding="15px 30px"
+    />,
+    <Banner
+      key="banner5"
+      texto="Oferta especial: 50% de descuento"
+      backgroundColor="#FF5722"
+      textColor="#FFFFFF"
+      fontSize="2rem"
+      padding="15px 30px"
+    />,
+    <Banner
+      key="banner6"
+      texto="Oferta especial: 50% de descuento"
+      backgroundColor="#FF5722"
+      textColor="#FFFFFF"
+      fontSize="2rem"
+      padding="15px 30px"
+    />,
+    <Banner
+      key="banner7"
+      texto="Oferta especial: 50% de descuento"
+      backgroundColor="#FF5722"
+      textColor="#FFFFFF"
+      fontSize="2rem"
+      padding="15px 30px"
+    />,
   ];
 
   const handleConfirmarCompra = () => {
@@ -185,13 +202,13 @@ const Kit4Camaras: React.FC = () => {
       <Beneficios />
 
       <Banner
-      key="banner1"
-      texto="Oferta especial: 50% de descuento"
-      backgroundColor="#FF5722"
-      textColor="#FFFFFF"
-      fontSize="2rem"
-      padding="15px 30px"
-    />,
+        key="banner0"
+        texto="Oferta especial: 50% de descuento"
+        backgroundColor="#FF5722"
+        textColor="#FFFFFF"
+        fontSize="2rem"
+        padding="15px 30px"
+      />
 
       {/* Renderizar carruseles y banners intercalados */}
       {carruseles.map((carrusel, index) => (
@@ -212,18 +229,13 @@ const Kit4Camaras: React.FC = () => {
         </React.Fragment>
       ))}
 
-<HeroHeader
+      <HeroHeader
         imagen={headerImage}
-        titulo="solo para Bucaramanga y el área metropolitana"
-        subtitulo="tecnologia acusense deteccion humanos y vehiculos!"
-        descripcion="kit de 4 cámaras Full HD 1080p (2mpx) de HIKVISION con grabacion a 4 mpx lite, ideales para todo tipo de negocio, casa u oficina."
+        titulo=""
+        subtitulo=""
+        descripcion=""
       >
-        <BotonCompra
-          texto="Comprar Kit de 4 Cámaras"
-          onClick={abrirModal}
-          color="#28a745"
-          tamaño="mediano"
-        />
+      
       </HeroHeader>
 
       <Formulario
