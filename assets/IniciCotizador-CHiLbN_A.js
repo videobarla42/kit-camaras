af90348769251f480a6a60bed6dcddceddeb521a
fernando <s24h@outlook.es>
2025-02-06 10:46:25 -0500

    Updates

--- assets/IniciCotizador-CHiLbN_A.js
+++ assets/IniciCotizador-CHiLbN_A.js
@@ -1,4 +1,4 @@
-import{j as n,r as $,a as _e,g as Fe,b as Le,c as ke}from"./index-B7qrZsZZ.js";import{I as Se}from"./1-1m3KLFQR.js";import{d as Ee}from"./2-CoIMD5PH.js";import{c as Ue}from"./createLucideIcon-xdb94J2O.js";/**
+import{j as n,r as $,a as _e,g as Fe,b as Le,c as ke}from"./index-CV8kMLeM.js";import{I as Se}from"./1-1m3KLFQR.js";import{d as Ee}from"./2-CoIMD5PH.js";import{c as Ue}from"./createLucideIcon-DODfFrZ-.js";/**
  * @license lucide-react v0.474.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/createLucideIcon-DODfFrZ-.js
+++ assets/createLucideIcon-DODfFrZ-.js
@@ -1,4 +1,4 @@
-import{r as a}from"./index-B7qrZsZZ.js";/**
+import{r as a}from"./index-CV8kMLeM.js";/**
  * @license lucide-react v0.474.0 - ISC
  *
  * This source code is licensed under the ISC license.
